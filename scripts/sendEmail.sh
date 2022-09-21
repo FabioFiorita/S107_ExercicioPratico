8700cdbb4ae52ebed27caea542c94700c5faacb1
Fabio Fiorita <49520710+FabioFiorita@users.noreply.github.com>
2022-09-21 18:35:26 -0300

    Update sendEmail.sh

--- scripts/sendEmail.sh
+++ scripts/sendEmail.sh
@@ -1,2 +1,2 @@
 sudo apt install mailutils
-mail -s 'Situação do Pipeline' "$EMAIL" <<< 'Pipeline executado com sucesso!'
+mail -s 'Situação do Pipeline' fabiolfp@gmail.com <<< 'Pipeline executado com sucesso!'
